15f2beb90c60d93bdfc0f33da577b969155c8b55
AGNIKT Technology <agnikt@gmail.com>
2017-09-16 06:10:02 +0530

    To category dynamic in mega menu

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -15,6 +15,7 @@ class ControllerCommonHome extends Controller {
 		$data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
+		$data['HTTP_SERVER'] = HTTP_SERVER;
 
 		$this->response->setOutput($this->load->view('common/home', $data));
 	}
