5de794f44f55d3e53e339c499b8d040b23849c2f
paulkumar009 <paulkumar.nadar@diamondsfactory.in>
2017-11-11 13:28:08 +0530

    worked on given changes

--- catalog/view/javascript/hcode/main.js
+++ catalog/view/javascript/hcode/main.js
@@ -300,8 +300,8 @@ $(document).ready(function () {
     });
 	$("#filter-products").owlCarousel({
         navigation: true,
-        items: 9,
-        itemsDesktop: [1200, 9],
+        items: 7,
+        itemsDesktop: [1200, 7],
         itemsTablet: [800, 6],
         itemsMobile: [700, 3],
         navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
