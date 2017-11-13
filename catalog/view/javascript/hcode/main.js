b2efeb69b33a211fd6083fc60f2da0770a0b8902
paulkumar009 <paulkumar.nadar@diamondsfactory.in>
2017-11-13 18:50:18 +0530

    mobile filer added

--- catalog/view/javascript/hcode/main.js
+++ catalog/view/javascript/hcode/main.js
@@ -302,8 +302,8 @@ $(document).ready(function () {
         navigation: true,
         items: 7,
         itemsDesktop: [1200, 7],
-        itemsTablet: [800, 6],
-        itemsMobile: [700, 3],
+        itemsTablet: [800, 3],
+        itemsMobile: [700, 1],
         navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
     });
     $("#owl-demo-brand").owlCarousel({
